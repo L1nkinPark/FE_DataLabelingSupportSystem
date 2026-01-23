1bd9c5b99b4ffec2f514d8f73030fa5a922c64e2
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-01-23 15:22:28 +0700

    Clean up

--- backend/DAL/Interfaces/ILabelRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DTOs.Entities;
-
-namespace DAL.Interfaces
-{
-    public interface ILabelRepository : IRepository<LabelClass>
-    {
-        Task<bool> ExistsInProjectAsync(int projectId, string labelName);
-    }
-}
