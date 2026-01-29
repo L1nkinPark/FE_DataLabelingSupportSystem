4f169adc5f10d4ff19d02d52a29b8b294ceff5b5
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-01-30 02:15:35 +0700

    Fix_Bug_3

--- src/page/auth/login/LoginPage.jsx
+++ src/page/auth/login/LoginPage.jsx
@@ -3,8 +3,8 @@ import AuthRight from "../../../components/auth/auth-right/AuthRight";
 
 const LoginPage = () => {
   return (
-    <div className="auth-page-wrapper bg-light d-flex align-items-center justify-content-center min-vh-100 py-4">
-      <div className="container">
+    <div className="auth-page-wrapper bg-light d-block d-lg-flex align-items-lg-center justify-content-center min-vh-100 py-0 py-lg-4">
+      <div className="container p-0 p-lg-3">
         <div className="row justify-content-center g-0">
           <div className="col-xl-9 col-lg-10 col-md-11 p-0">
             {/* Thẻ card sẽ tự co giãn theo nội dung bên trong, tránh dính đỉnh */}
